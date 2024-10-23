d4c59de4745009abd4a63d13543d694458db4c68
Viktoria Dolgolevets <vikadolgolevec@gmail.com>
2024-10-23 22:08:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+pol = 0 #счетчик положительных
+otr = 0 #счетчик отрицательных
+zero = 0 #счетчик нулевых
+number = [random.randint(-50, 50) for i in range(25)] #создает список
+print("список: ", number) #выводится список
+for i in number: #счет пол отр и нул элементов
+    if i > 0:
+        pol += 1
+    elif i < 0:
+        otr += 1
+    else:
+        zero += 1
+print(f"положительные элементы:  {pol}, {float(pol)/25*100:.0f}%") #вывод колва пол знач
+print(f"отрицательные элементы:  {otr}, {float(otr)/25*100:.0f}%") #вывод колва отр знач
+print(f"нулевые  элементы:  {zero}, {float(zero)/25*100:.0f}%") #вывод колва 0 знач
+print(f"мин значение:  {min(number)}", sep="\n") #вывод мин
+print(f"макс значение:  {max(number)}", sep="\n") #вывод макс
